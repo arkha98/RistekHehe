666dfa1a3ad7e319aec87b53653b0ecb3e930ac1
arkha98 <arkhasayoga.m@gmail.com>
2017-09-07 03:34:39 +0700

    edit lcd.py

--- lcd.py
+++ lcd.py
@@ -3,3 +3,20 @@ suhu = 0
 berat = 0
 nama_makanan = ""
 gizi = []
+
+def cetakSuhu():
+	print(suhu)
+	return 0
+
+def cetakBerat():
+	print(berat)
+	return 0
+
+def cetakNamaMakanan():
+	print(nama_makanan)
+	return 0
+
+def cetakGizi():
+	for (i in gizi):
+		print
+	return 0
